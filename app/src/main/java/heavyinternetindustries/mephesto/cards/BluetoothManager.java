78a50c6eed7b0c4d3d6cca4083e4234f94b35662
petcon <pco043@student.uib.no>
2016-02-09 14:01:57 +0100

    Added the class BluetoothManager to handle all Bluetooth (not BLE) actions. Also removed Bluetooth code from MainActivity.

---
+++ app/src/main/java/heavyinternetindustries/mephesto/cards/BluetoothManager.java
@@ -0,0 +1,140 @@
+package heavyinternetindustries.mephesto.cards;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.util.Pair;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+/**
+ * Created by mephest0 on 09.02.16.
+ */
+public class BluetoothManager {
+    BluetoothAdapter bluetoothAdapter;
+    ArrayList<BluetoothDevice> discoveredDevices;
+    MainActivity activity;
+    private boolean isScanning;
+
+    public BluetoothManager(MainActivity activity) {
+        this.activity = activity;
+        isScanning = false;
+        discoveredDevices = new ArrayList<>();
+
+        //find BluetoothManager
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        Toast toast;
+        if (bluetoothAdapter == null) {
+            //no bluetooths
+            toast = Toast.makeText(activity.getApplicationContext(), "No bluetooth", Toast.LENGTH_LONG);
+        } else {
+            //all good
+            toast = Toast.makeText(activity.getApplicationContext(), "Bluetooth initialized", Toast.LENGTH_SHORT);
+        }
+        toast.show();
+    }
+
+    public void onResume() {
+
+    }
+
+    public void onPause() {
+        //stuff goes here
+
+        if (isScanning()) activity.unregisterReceiver(mReceiver);
+    }
+
+    public boolean isEnabled() {
+        return bluetoothAdapter.isEnabled();
+    }
+
+    /**
+     * Initiates a scan. Discovered devices are stored in a list.
+     * @See getDiscoveredDevices()
+     */
+    ArrayList<Pair<String, String>> scanForDevices() {
+        System.out.println("scanForDevices()");
+
+        if (isEnabled()) {
+            System.out.println("scanning for devices");
+            IntentFilter filter = new IntentFilter();
+
+            filter.addAction(BluetoothDevice.ACTION_FOUND);
+            filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+            filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+
+            activity.registerReceiver(mReceiver, filter);
+
+            bluetoothAdapter.startDiscovery();
+        } else {
+            System.out.println("bluetooth not enabled");
+            return new ArrayList<>();
+        }
+
+        return null;
+    }
+
+    ArrayList<BluetoothDevice> getDiscoveredDevices() {
+        return discoveredDevices;
+    }
+
+    public void makeDiscoverable() {
+        System.out.println("make discoverable");
+        Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 120);
+        activity.startActivity(discoverableIntent);
+    }
+
+    public void checkIfBluetoothEnabled() {
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            activity.startActivityForResult(enableBtIntent, 123); //Request code (for activity result) goes here
+        } else {
+
+        }
+    }
+
+    public boolean isScanning() {
+        return isScanning;
+    }
+
+    private void scanStarted() {
+        System.out.println("Starting scan");
+        isScanning = true;
+        discoveredDevices.clear();
+    }
+
+    private void scanFinished() {
+        System.out.println("Scan finished");
+        isScanning = false;
+        activity.unregisterReceiver(mReceiver);
+
+        System.out.println("Printing discovered devices:");
+        for (BluetoothDevice device : discoveredDevices)
+            System.out.println(" + " + device.getName());
+        System.out.println("-");
+    }
+
+    //Intent receiver
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            if (BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action)) {
+                scanStarted();
+            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+                scanFinished();
+            } else if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                //bluetooth device found
+                BluetoothDevice device = (BluetoothDevice) intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+                discoveredDevices.add(device);
+                System.out.println("Found device " + device.getName());
+            }
+        }
+    };
+}
--- app/src/main/java/heavyinternetindustries/mephesto/cards/MainActivity.java
+++ app/src/main/java/heavyinternetindustries/mephesto/cards/MainActivity.java
@@ -8,35 +8,22 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Stack;
+
 public class MainActivity extends ActionBarActivity {
+    Stack<Pair<String, String>> messages;
     TextView debMessages;
-    BluetoothAdapter bluetoothAdapter;
 
-    //Intent receiver
-    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-
-            if (BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action)) {
-                System.out.println("Starting scan");
-                setScanStarted();
-            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
-                System.out.println("Scan finished");
-                setScanFinished();
-            } else if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                //bluetooth device found
-                BluetoothDevice device = (BluetoothDevice) intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-
-                System.out.println("Found device " + device.getName());
-            }
-        }
-    };
+    BluetoothManager bluetoothManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,26 +33,10 @@ public class MainActivity extends ActionBarActivity {
         //find TextView
         debMessages = (TextView) findViewById(R.id.debug_messages);
 
-        //find BluetoothManager
-        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
-        Toast toast;
-        if (bluetoothAdapter == null) {
-            //no bluetooths
-            toast = Toast.makeText(getApplicationContext(), "No bluetooth", Toast.LENGTH_LONG);
-        } else {
-            //all good
-            toast = Toast.makeText(getApplicationContext(), "Bluetooth initialized", Toast.LENGTH_SHORT);
-        }
-        toast.show();
-    }
+        bluetoothManager = new BluetoothManager(this);
 
-    public void setScanStarted() {
-        ((Button) findViewById(R.id.scan)).setEnabled(false);
-    }
-
-    public void setScanFinished() {
-        ((Button) findViewById(R.id.scan)).setEnabled(true);
+        messages = new Stack<>();
     }
 
     public void submitName(View view) {
@@ -83,49 +54,38 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void makeDiscoverable(View view) {
-        makeDiscoverable();
-    }
-
-    public void makeDiscoverable() {
-        System.out.println("make discoverable");
-        Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 120);
-        startActivity(discoverableIntent);
-        System.out.println("made discoverable");
+        bluetoothManager.makeDiscoverable();
     }
 
     public void scanForDevices(View view) {
         checkIfBluetoothEnabled();
 
-        IntentFilter filter = new IntentFilter();
-
-        filter.addAction(BluetoothDevice.ACTION_FOUND);
-        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
-        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
-
-        registerReceiver(mReceiver, filter);
-        bluetoothAdapter.startDiscovery();
-    }
-
-    public void startNewHost(String name) {
-        //TODO
+        if (!bluetoothManager.isScanning()) {
+            bluetoothManager.scanForDevices();
+        } else {
+            System.out.println("Printing devices:");
+            debMessages.setText("");
+            String devList = "";
+            for (BluetoothDevice device : bluetoothManager.getDiscoveredDevices()) {
+                System.out.println(" + " + device.getName());
+                devList += device.getName() + "\n";
+            }
+            debMessages.setText(devList);
+            System.out.println("+");
+        }
     }
 
     public void checkIfBluetoothEnabled() {
-        if (!bluetoothAdapter.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, 123); //Request code goes here
-        }
-
+        bluetoothManager.checkIfBluetoothEnabled();
     }
 
     @Override
     public void onPause() {
-        unregisterReceiver(mReceiver);
         super.onPause();
+        bluetoothManager.onPause();
     }
 
-    public void bacon(){
-        System.out.println("tasty tasty");
+    public void toast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
